ff54c5ab1709df573fb368bb68af893874a08054
joav-sha <joaovitor8832@gmail.com>
2024-04-15 12:48:32 -0300

    bee1004 realizado com sucesso

---
+++ Java/1 - Iniciante/bee1004.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class bee1004 {
+    
+    public static Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args){
+        System.out.printf("PROD = %d\n", (s.nextInt() * s.nextInt()));
+    }
+}
