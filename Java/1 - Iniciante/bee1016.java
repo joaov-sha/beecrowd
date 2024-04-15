8f7753f27ab4333921a7fc21153dd8ef4fdb914f
joav-sha <joaovitor8832@gmail.com>
2024-04-15 14:09:25 -0300

    bee1016 realizada com sucesso

---
+++ Java/1 - Iniciante/bee1016.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class bee1016 {
+    
+    public static Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args){
+        System.out.printf("%.0f minutos\n",(s.nextInt()/0.5));
+    }
+}
