50e36037be9c18e1a2a0364caa82b80e58c942c1
joav-sha <joaovitor8832@gmail.com>
2024-04-15 12:42:00 -0300

    bee1002 realizado com sucesso

---
+++ Java/1 - Iniciante/bee1002.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class bee1002 {
+    
+    public static Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args){
+        double pi = 3.14159;
+        System.out.printf("A=%.4f\n",(pi*Math.pow(s.nextDouble(),2)));
+    }
+}
