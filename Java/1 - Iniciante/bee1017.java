4b12a6d35cf3ccef62677f20b3abfca79531024c
joav-sha <joaovitor8832@gmail.com>
2024-04-15 14:13:56 -0300

    bee1017 realizada com sucesso

---
+++ Java/1 - Iniciante/bee1017.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class bee1017 {
+    
+    public static Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args){
+        System.out.printf("%.3f\n",((double)s.nextInt()/12*s.nextInt()));
+    }
+}
