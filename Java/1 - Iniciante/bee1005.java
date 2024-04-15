4f72a448f7ca5fc7f4e023af9888e226720b91df
joav-sha <joaovitor8832@gmail.com>
2024-04-15 12:54:16 -0300

    bee1005 realizado com sucesso

---
+++ Java/1 - Iniciante/bee1005.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class bee1005 {
+    
+    public static Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args){
+        System.out.printf("MEDIA = %.5f\n", ((s.nextDouble()*3.5 + s.nextDouble()*7.5)/11));
+    }
+}
