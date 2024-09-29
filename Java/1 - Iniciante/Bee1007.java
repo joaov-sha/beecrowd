bb2543989b5701137abdae4bd9fb922e058972e4
joaov-sha <joaovitor8832@gmail.com>
2024-09-29 15:42:12 -0300

    Bee1007 realizada com sucesso

---
+++ Java/1 - Iniciante/Bee1007.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Bee1007 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.printf("DIFERENCA = %d%n",(s.nextInt()*s.nextInt()-s.nextInt()*s.nextInt()));
+        s.close();
+    }
+}
