326554c9af67aa4afb8cccaa03da060641575b3e
joaov-sha <joaovitor8832@gmail.com>
2024-09-29 16:18:33 -0300

    Bee1014 realizada com sucesso

--- Java/1 - Iniciante/Bee1014.java
+++ Java/1 - Iniciante/Bee1014.java
@@ -1,3 +1,9 @@
+import java.util.Scanner;
+
 public class Bee1014 {
-    
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.printf("%.3f km/l%n",(s.nextInt()/s.nextDouble()));
+        s.close();
+    }
 }
