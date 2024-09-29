54c0ecd50e2b0a43d2787f4e5338f9fc1e2540b7
joaov-sha <joaovitor8832@gmail.com>
2024-09-29 17:57:09 -0300

    Bee1019 realizada com sucesso

---
+++ Java/1 - Iniciante/Bee1019.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Bee1019 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        System.out.printf("%d:%d:%d%n",(n/3600),(n%3600)/60,(n%60));
+        s.close();
+    }
+}
