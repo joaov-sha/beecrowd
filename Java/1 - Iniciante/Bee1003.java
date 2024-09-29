64e5428d8f6e6b47ff7df6c201beda1d476f5042
joaov-sha <joaovitor8832@gmail.com>
2024-09-29 15:31:32 -0300

    Bee1003 realizada com sucesso

---
+++ Java/1 - Iniciante/Bee1003.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Bee1003 {
+    public static Scanner s = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.printf("SOMA = %d%n",(s.nextInt() + s.nextInt()));
+    }
+}
