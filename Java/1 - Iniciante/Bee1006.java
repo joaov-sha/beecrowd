76c5795737c1f0c8ac8f7e71124a12904834e287
joaov-sha <joaovitor8832@gmail.com>
2024-09-29 15:39:51 -0300

    Bee1006 realizada com sucesso

---
+++ Java/1 - Iniciante/Bee1006.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Bee1006 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.printf("MEDIA = %.1f%n",(((s.nextDouble()*2)+(s.nextDouble()*3)+(s.nextDouble()*5))/10));
+        s.close();
+    }
+}
