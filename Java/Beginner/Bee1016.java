01bbfc3ee2bc72f49fb668531961c9ad8e9631fc
joaov-sha <joaovitor8832@gmail.com>
2025-02-15 01:53:30 -0300

    Bee1016 resolvido com sucesso

--- Java/Beginner/Bee1016.java
+++ Java/Beginner/Bee1016.java
@@ -5,7 +5,7 @@ public class Bee1016{
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
         int distancia = s.nextInt();
-        System.out.printf("%.0f minutos", (distancia/0.5));
+        System.out.printf("%.0f minutos\n", (distancia/0.5));
         s.close();
     }
 }
