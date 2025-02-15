d26d5968debce5aba7ae6929ed6ba89e69affe32
joaov-sha <joaovitor8832@gmail.com>
2025-02-15 00:49:23 -0300

    Bee1002 resolvido com sucesso

--- Java/Beginner/Bee1002.java
+++ Java/Beginner/Bee1002.java
@@ -7,7 +7,8 @@ public class Bee1002 {
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
         double raio = s.nextDouble();
-        double area = Math.PI*Math.pow(raio,2);
+        double pi = 3.14159;
+        double area = pi*Math.pow(raio,2);
         System.out.printf("A=%.4f\n", area);
         s.close();
     }
