5a4be75fcc174abc9987a1257ed97128a03c393a
joaov-sha <joaovitor8832@gmail.com>
2025-02-15 12:55:17 -0300

    Bee1018 resolvido com sucesso

---
+++ Java/Beginner/Bee1018.java
@@ -0,0 +1,26 @@
+package Java.Beginner;
+
+import java.util.Scanner;
+public class Bee1018{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int valor, nCem, nCinquenta, nVinte, nDez, nCinco, nDois, nUm;
+        valor = s.nextInt();
+        nCem = valor / 100;
+        nCinquenta = (valor % 100) / 50;
+        nVinte = ((valor % 100) % 50) / 20;
+        nDez = (((valor % 100) % 50) % 20) / 10;
+        nCinco = ((((valor % 100) % 50) % 20) % 10) / 5;
+        nDois = (((((valor % 100) % 50) % 20) % 10) % 5 ) / 2;
+        nUm = ((((((valor % 100) % 50) % 20) % 10) % 5) % 2) / 1;
+        System.out.println(valor);
+        System.out.println(nCem + " nota(s) de R$ 100,00");
+        System.out.println(nCinquenta + " nota(s) de R$ 50,00");
+        System.out.println(nVinte + " nota(s) de R$ 20,00");
+        System.out.println(nDez + " nota(s) de R$ 10,00");
+        System.out.println(nCinco + " nota(s) de R$ 5,00");
+        System.out.println(nDois + " nota(s) de R$ 2,00");
+        System.out.println(nUm + " nota(s) de R$ 1,00");
+        s.close();
+    }
+}
