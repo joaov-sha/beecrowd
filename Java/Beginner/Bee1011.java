a3a3c21e44e1c3d1fc0e3cbe6fcf7b98aa3e9211
joaov-sha <joaovitor8832@gmail.com>
2025-02-17 18:14:03 -0300

    Adicionada a descrição do problema Bee1011

--- Java/Beginner/Bee1011.java
+++ Java/Beginner/Bee1011.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class Bee1011{
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
+        // Descrição: Faça um programa que calcule e mostre o volume de uma esfera sendo fornecido o valor de seu raio (R). A fórmula para calcular o volume é: (4/3) * pi * R3. Considere (atribua) para pi o valor 3.14159.
         double R, pi, volume;
         pi = 3.14159;
         R = s.nextDouble();
