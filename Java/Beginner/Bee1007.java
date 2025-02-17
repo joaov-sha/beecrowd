9fef77a406fbfd0a3b2e3e61523a4f113d487281
joaov-sha <joaovitor8832@gmail.com>
2025-02-17 18:13:21 -0300

    Adicionada a descrição do problema Bee1007

--- Java/Beginner/Bee1007.java
+++ Java/Beginner/Bee1007.java
@@ -3,6 +3,7 @@ package Java.Beginner;
 import java.util.Scanner;
 public class Bee1007{
     public static void main(String[] args){
+        // Descrição: Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
         Scanner s = new Scanner(System.in);
         int A,B,C,D, diferenca;
         A = s.nextInt();
