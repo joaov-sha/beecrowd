a7f3164bc8c7a5f367c71d2662e220c6e276be13
joaov-sha <joaovitor8832@gmail.com>
2025-02-17 18:12:18 -0300

    Adicionada a descrição do problema Bee1001

--- Java/Beginner/Bee1001.java
+++ Java/Beginner/Bee1001.java
@@ -2,8 +2,8 @@ package Java.Beginner;
 
 import java.util.Scanner;
 public class Bee1001 {
-    
     public static void main(String[] args){
+        // Descrição: Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B atribuindo o seu resultado na variável X. Imprima X conforme exemplo apresentado abaixo. Não apresente mensagem alguma além daquilo que está sendo especificado e não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
         Scanner s = new Scanner(System.in);
         int A,B,X;
         A = s.nextInt();
