8f1a2ae81b570c4b0610ca3eb42969751f401d41
joaov-sha <joaovitor8832@gmail.com>
2025-02-17 18:13:43 -0300

    Adicionada a descrição do problema Bee1009

--- Java/Beginner/Bee1009.java
+++ Java/Beginner/Bee1009.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class Bee1009{
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
+        // Descrição: Faça um programa que leia o nome de um vendedor, o seu salário fixo e o total de vendas efetuadas por ele no mês (em dinheiro). Sabendo que este vendedor ganha 15% de comissão sobre suas vendas efetuadas, informar o total a receber no final do mês, com duas casas decimais.
         String nome;
         double salario, vendas;
         nome = s.nextLine();
