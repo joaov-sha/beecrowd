a21a1903a59f7d8156d801510032bc8f4e0a3c61
joaov-sha <joaovitor8832@gmail.com>
2025-02-17 18:14:23 -0300

    Adicionada a descrição do problema Bee1013

--- Java/Beginner/Bee1013.java
+++ Java/Beginner/Bee1013.java
@@ -3,6 +3,7 @@ package Java.Beginner;
 import java.util.Scanner;
 public class Bee1013{
     public static void main(String[] args){
+        // Descrição: Faça um programa que leia três valores e apresente o maior dos três valores lidos seguido da mensagem “eh o maior”. 
         Scanner s = new Scanner(System.in);
         int A,B,C, maiorAB, maiorC;
         A = s.nextInt();
