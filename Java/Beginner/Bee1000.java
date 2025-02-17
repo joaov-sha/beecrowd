85893dfbda17d37ba1ced2363b75c74b116ddff0
joaov-sha <joaovitor8832@gmail.com>
2025-02-17 18:12:06 -0300

    Adicionada a descrição do problema Bee1000

--- Java/Beginner/Bee1000.java
+++ Java/Beginner/Bee1000.java
@@ -2,6 +2,7 @@ package Java.Beginner;
 
 public class Bee1000{
     public static void main(String[] args){
+        // Descrição: O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
         System.out.println("Hello World!");
     }
 }
