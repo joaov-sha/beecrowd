ec57c463f3e63cf5c75f732062d97db91a793b46
joaov-sha <joaovitor8832@gmail.com>
2025-02-17 18:12:40 -0300

    Adicionada a descrição do problema Bee1003

--- Java/Beginner/Bee1003.java
+++ Java/Beginner/Bee1003.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Bee1003 {
     
     public static void main(String[] args){
+        // Descrição: Leia dois valores inteiros, no caso para variáveis A e B. A seguir, calcule a soma entre elas e atribua à variável SOMA. A seguir escrever o valor desta variável.
         Scanner s = new Scanner(System.in);
         int A, B, SOMA;
         A = s.nextInt();
