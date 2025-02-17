a7f4ad2cfe61ef6e36694fe856e8b09999ec46e6
joaov-sha <joaovitor8832@gmail.com>
2025-02-17 15:03:19 -0300

    Bee1035 - versão 1 - falha 25%

---
+++ Java/Beginner/Bee1035.java
@@ -0,0 +1,19 @@
+package Java.Beginner;
+
+import java.util.Scanner;
+public class Bee1035{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int A, B, C, D;
+        A = s.nextInt();
+        B = s.nextInt();
+        C = s.nextInt();
+        D = s.nextInt();
+        if(B > C && D > A && (C+D) > (A+B) && C > 0 && D > 0 && A % 2 != 0){
+            System.out.println("Valores aceitos");
+        }else{
+            System.out.println("Valores nao aceitos");
+        }
+        s.close();
+    }
+}
