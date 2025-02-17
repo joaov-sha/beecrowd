69a7fef7710bf2647a07a782e45b6fe18abe44ce
joaov-sha <joaovitor8832@gmail.com>
2025-02-17 18:13:54 -0300

    Adicionada a descrição do problema Bee1010

--- Java/Beginner/Bee1010.java
+++ Java/Beginner/Bee1010.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class Bee1010{
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
+        // Descrição: Neste problema, deve-se ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. Após, calcule e mostre o valor a ser pago.
         int codPUm, quantPUm, codPDois, quantPDois;
         double vPUm, vPDois;
         codPUm = s.nextInt();
