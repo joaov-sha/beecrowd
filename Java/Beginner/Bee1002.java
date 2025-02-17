ee590b77d9e4e2a58f1e5aeac5777f286b1a9229
joaov-sha <joaovitor8832@gmail.com>
2025-02-17 18:12:28 -0300

    Adicionada a descrição do problema Bee1002

--- Java/Beginner/Bee1002.java
+++ Java/Beginner/Bee1002.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 public class Bee1002 {
     
     public static void main(String[] args){
+        /*
+            Descrição:
+            A fórmula para calcular a área de uma circunferência é: area = π . raio2. Considerando para este problema que π = 3.14159:
+            - Efetue o cálculo da área, elevando o valor de raio ao quadrado e multiplicando por π.
+        */
         Scanner s = new Scanner(System.in);
         double raio = s.nextDouble();
         double pi = 3.14159;
