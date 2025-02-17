aa149f70c82fa2829a9be44edc6c331715269fff
joaov-sha <joaovitor8832@gmail.com>
2025-02-17 18:12:51 -0300

    Adicionada a descrição do problema Bee1004

--- Java/Beginner/Bee1004.java
+++ Java/Beginner/Bee1004.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Bee1004 {
     
     public static void main(String[] args){
+        // Descrição: Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD. A seguir mostre a variável PROD com mensagem correspondente.
         Scanner s = new Scanner(System.in);
         int a, b, prod;
         a = s.nextInt();
