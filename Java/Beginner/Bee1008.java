1a778c7486bc16a262a8fd78550397fa4cd63c7a
joaov-sha <joaovitor8832@gmail.com>
2025-02-17 18:13:32 -0300

    Adicionada a descrição do problema Bee1008

--- Java/Beginner/Bee1008.java
+++ Java/Beginner/Bee1008.java
@@ -3,6 +3,7 @@ package Java.Beginner;
 import java.util.Scanner;
 public class Bee1008{
     public static void main(String[] args){
+        // Descrição: Escreva um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
         Scanner s = new Scanner(System.in);
         int num, horas;
         double valor;
