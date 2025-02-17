6162ff74bb3ef7db68ca90eaf44308299a3bb810
joaov-sha <joaovitor8832@gmail.com>
2025-02-17 18:14:31 -0300

    Adicionada a descrição do problema Bee1014

--- Java/Beginner/Bee1014.java
+++ Java/Beginner/Bee1014.java
@@ -3,6 +3,7 @@ package Java.Beginner;
 import java.util.Scanner;
 public class Bee1014{
     public static void main(String[] args){
+        // Descrição: Calcule o consumo médio de um automóvel sendo fornecidos a distância total percorrida em KM e o gasto de combustível em litros
         Scanner s = new Scanner(System.in);
         int X = s.nextInt();
         double Y = s.nextDouble();
