f78b6cd8c6a7688f55d9ff7648249e0118a91af8
joaov-sha <joaovitor8832@gmail.com>
2025-02-17 17:48:35 -0300

    Bee1041 resolvido com sucesso

--- Java/Beginner/Bee1041.java
+++ Java/Beginner/Bee1041.java
@@ -17,6 +17,10 @@ public class Bee1041{
             System.out.println("Q4");
         }else if(x == y && x == 0){
             System.out.println("Origem");
+        }else if(x != 0 && y == 0){
+            System.out.println("Eixo X");
+        }else if(x == 0 && y != 0){
+            System.out.println("Eixo Y");
         }
         s.close();
     }
