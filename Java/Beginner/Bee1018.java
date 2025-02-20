15290f5c246ff9aa00c48e1c3f9af16d0cdf5fea
joaov-sha <joaovitor8832@gmail.com>
2025-02-19 23:08:19 -0300

    Adicionada a descrição do problema Bee1018

--- Java/Beginner/Bee1018.java
+++ Java/Beginner/Bee1018.java
@@ -3,6 +3,9 @@ package Java.Beginner;
 import java.util.Scanner;
 public class Bee1018{
     public static void main(String[] args){
+
+        // Descrição: Leia um valor inteiro. A seguir, calcule o menor número de notas possíveis (cédulas) no qual o valor pode ser decomposto. As notas consideradas são de 100, 50, 20, 10, 5, 2 e 1. A seguir mostre o valor lido e a relação de notas necessárias.
+        
         Scanner s = new Scanner(System.in);
         int valor, nCem, nCinquenta, nVinte, nDez, nCinco, nDois, nUm;
         valor = s.nextInt();
