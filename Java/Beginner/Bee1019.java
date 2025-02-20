d9a92640af93c616b65d0e9cbf9d75a2e045202a
joaov-sha <joaovitor8832@gmail.com>
2025-02-19 23:09:10 -0300

    Adicionada a descrição do problema Bee1019

--- Java/Beginner/Bee1019.java
+++ Java/Beginner/Bee1019.java
@@ -3,6 +3,9 @@ package Java.Beginner;
 import java.util.Scanner;
 public class Bee1019{
     public static void main(String[] args){
+
+        // Descrição: Leia um valor inteiro, que é o tempo de duração em segundos de um determinado evento em uma fábrica, e informe-o expresso no formato horas:minutos:segundos.
+
         Scanner s = new Scanner(System.in);
         int N, horas, minutos, segundos;
         N = s.nextInt();
