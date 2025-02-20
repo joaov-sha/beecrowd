376c66db5cb56897205bc4196a78391390a8dd6e
joaov-sha <joaovitor8832@gmail.com>
2025-02-19 23:46:41 -0300

    Bee1043 - versão 1 - falha 40%

---
+++ Java/Beginner/Bee1043.java
@@ -0,0 +1,22 @@
+package Java.Beginner;
+
+import java.util.Scanner;
+public class Bee1043{
+    public static void main(String[] args){
+
+        // Descrição: Leia 3 valores reais (A, B e C) e verifique se eles formam ou não um triângulo. Em caso positivo, calcule o perímetro do triângulo e apresente a mensagem: Perimetro = XX.X .Em caso negativo, calcule a área do trapézio que tem A e B como base e C como altura, mostrando a mensagem: Area = XX.X
+
+        Scanner s = new Scanner(System.in);
+        double a,b,c;
+        a = s.nextDouble();
+        b = s.nextDouble();
+        c = s.nextDouble();
+        if(a < (b+c) || b < (a+c) || c < (a+b)){
+            System.out.printf("Perimetro = %.1f\n", (a+b+c));
+        }else{
+            System.out.printf("Area = %.1f\n", (((a+b)*c)/2));
+        }
+        
+        s.close();
+    }
+}
