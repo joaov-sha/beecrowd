cfb7ab624d86fe3b33a5f60eb788918e34adcbf9
joaov-sha <joaovitor8832@gmail.com>
2025-02-19 23:15:25 -0300

    Adicionada a descrição do problema Bee1038

--- Java/Beginner/Bee1038.java
+++ Java/Beginner/Bee1038.java
@@ -3,6 +3,16 @@ package Java.Beginner;
 import java.util.Scanner;
 public class Bee1038{
     public static void main(String[] args){
+        
+        // Descrição: Com base na tabela abaixo, escreva um programa que leia o código de um item e a quantidade deste item. A seguir, calcule e mostre o valor da conta a pagar.
+        /* Codigo   Especificacao   Preco
+         * 1        Cachorro Quente R$ 4.00
+         * 2        X-Salada        R$ 4.50
+         * 3        X-Bacon         R$ 5.00
+         * 4        Torrada simples R$ 2.00
+         * 5        Refrigerante    R$ 1.50
+        */
+
         Scanner s = new Scanner(System.in);
         int codigo, quantidade;
         double valor, total;
