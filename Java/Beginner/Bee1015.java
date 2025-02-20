a28af72b08cc17167b013b72cd9f318819525868
joaov-sha <joaovitor8832@gmail.com>
2025-02-19 23:05:56 -0300

    Adicionada a descrição do problema Bee1015

--- Java/Beginner/Bee1015.java
+++ Java/Beginner/Bee1015.java
@@ -3,6 +3,9 @@ package Java.Beginner;
 import java.util.Scanner;
 public class Bee1015{
     public static void main(String[] args){
+
+        // Descrição: Leia os quatro valores correspondentes aos eixos x e y de dois pontos quaisquer no plano, p1(x1,y1) e p2(x2,y2) e calcule a distância entre eles, mostrando 4 casas decimais, segundo a fórmula: sqrt((x2-x1)²+(y2-y1²)).
+
         Scanner s = new Scanner(System.in);
         float x1,y1,x2,y2;
         x1 = s.nextFloat();
