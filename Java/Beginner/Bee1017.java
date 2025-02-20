04dde18f9bffe705f1b8253d16e9179f039bcda6
joaov-sha <joaovitor8832@gmail.com>
2025-02-19 23:07:47 -0300

    Adicionada a descrição do problema Bee1017

--- Java/Beginner/Bee1017.java
+++ Java/Beginner/Bee1017.java
@@ -3,6 +3,9 @@ package Java.Beginner;
 import java.util.Scanner;
 public class Bee1017{
     public static void main(String[] args){
+
+        // Descrição: Joaozinho quer calcular e mostrar a quantidade de litros de combustível gastos em uma viagem, ao utilizar um automóvel que faz 12 KM/L. Para isso, ele gostaria que você o auxiliasse através de um simples programa. Para efetuar o cálculo, deve-se fornecer o tempo gasto na viagem (em horas) e a velocidade média durante a mesma (em km/h). Assim, pode-se obter distância percorrida e, em seguida, calcular quantos litros seriam necessários. Mostre o valor com 3 casas decimais após o ponto.
+
         Scanner s = new Scanner(System.in);
         int tempo, velocidadeMedia;
         tempo = s.nextInt();
