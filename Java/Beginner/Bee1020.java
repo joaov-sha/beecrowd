44bd335972b438e5d68df29914bdd18c8eee7fdf
joaov-sha <joaovitor8832@gmail.com>
2025-02-19 23:10:03 -0300

    Adicionada a descrição do problema Bee1020

--- Java/Beginner/Bee1020.java
+++ Java/Beginner/Bee1020.java
@@ -3,6 +3,9 @@ package Java.Beginner;
 import java.util.Scanner;
 public class Bee1020{
     public static void main(String[] args){
+
+        // Descrição: Leia um valor inteiro correspondente à idade de uma pessoa em dias e informe-a em anos, meses e dias. Obs.: apenas para facilitar o cálculo, considere todo ano com 365 dias e todo mês com 30 dias. Nos casos de teste nunca haverá uma situação que permite 12 meses e alguns dias, como 360, 363 ou 364. Este é apenas um exercício com objetivo de testar raciocínio matemático simples.
+
         Scanner s = new Scanner(System.in);
         int valor, anos, meses, dias;
         valor = s.nextInt();
