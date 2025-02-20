d743892b203c588608781e1b5c2f8ac4405d286e
joaov-sha <joaovitor8832@gmail.com>
2025-02-19 23:17:08 -0300

    Adicionada a descrição do problema Bee1041

--- Java/Beginner/Bee1041.java
+++ Java/Beginner/Bee1041.java
@@ -3,6 +3,9 @@ package Java.Beginner;
 import java.util.Scanner;
 public class Bee1041{
     public static void main(String[] args){
+
+        // Descrição: Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas de um ponto em um plano. A seguir, determine qual o quadrante ao qual pertence o ponto, ou se está sobre um dos eixos cartesianos ou na origem (x = y = 0). Se o ponto estiver na origem, escreva a mensagem “Origem”. Se o ponto estiver sobre um dos eixos escreva “Eixo X” ou “Eixo Y”, conforme for a situação.
+        
         Scanner s = new Scanner(System.in);
         float x,y;
         x = s.nextFloat();
