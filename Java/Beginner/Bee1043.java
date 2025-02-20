0a59a9be1941063050ace067e062f3870bbb2c4d
joaov-sha <joaovitor8832@gmail.com>
2025-02-19 23:49:33 -0300

    Bee1043 - versão 3 - falha 40%

--- Java/Beginner/Bee1043.java
+++ Java/Beginner/Bee1043.java
@@ -12,9 +12,9 @@ public class Bee1043{
         b = s.nextDouble();
         c = s.nextDouble();
         if((a + b) > c|| (a + c) > b || (b + c) > a){
-            System.out.printf("Perimetro = %.1f\n", (a+b+c));
-        }else{
             System.out.printf("Area = %.1f\n", (((a+b)*c)/2));
+        }else{
+            System.out.printf("Perimetro = %.1f\n", (a+b+c));
         }
         
         s.close();
