3e2aa8ea374be90e2e1ba3903517d70f5d3b2ac6
joaov-sha <joaovitor8832@gmail.com>
2025-02-19 23:11:58 -0300

    Adicionada a descrição do problema Bee1036

--- Java/Beginner/Bee1036.java
+++ Java/Beginner/Bee1036.java
@@ -3,6 +3,9 @@ package Java.Beginner;
 import java.util.Scanner;
 public class Bee1036{
     public static void main(String[] args){
+
+        // Descrição: Leia 3 valores de ponto flutuante e efetue o cálculo das raízes da equação de Bhaskara. Se não for possível calcular as raízes, mostre a mensagem correspondente “Impossivel calcular”, caso haja uma divisão por 0 ou raiz de numero negativo.
+
         Scanner s = new Scanner(System.in);
         double a, b, c, delta, r1, r2;
         a = s.nextDouble();
